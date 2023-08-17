4ecf77c9d6180b8865fe11a3619331c3f750291d
phucbienvan <meotran0805@gmail.com>
2023-08-17 22:16:34 +0700

    update jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/users')
+export class UserController {
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('me')
+    me(@GetUser() user: User) {
+        return user;
+    }
+}
